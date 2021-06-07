33c7f26fa9a19d0e4938d199d9c91e16fae750c0
Omar Picado <ariielpb@gmail.com>
2021-06-07 12:28:16 -0600

    se modifica el contact_me.php

--- assets/mail/contact_me.php
+++ assets/mail/contact_me.php
@@ -11,7 +11,7 @@ $message = strip_tags(htmlspecialchars($_POST['message']));
 
 // Create the email and send the message
 $to = "ariielpb@gmail.com"; // Add your email address in between the "" replacing yourname@yourdomain.com - This is where the form will send a message to.
-$subject = "Correo de:  $name";
+$subject = "Correo de:  $name" + " - Residencia de Vida";
 $body = "Has recibido un nuevo correo de tu sitio web.\n\n"."Detalles:\n\Nombres: $name\n\nEmail: $email\n\nMensaje:\n$message";
 $header = "De residenciadevida-tcu\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
 $header .= "Reply-To: $email";
