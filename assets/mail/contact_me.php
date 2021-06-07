c4b674270e403bea5c214239fbe1e060e7c20d9f
Omar Picado <ariielpb@gmail.com>
2021-06-07 11:49:31 -0600

    se intentó reparar el correo

--- assets/mail/contact_me.php
+++ assets/mail/contact_me.php
@@ -10,12 +10,11 @@ $email = strip_tags(htmlspecialchars($_POST['email']));
 $message = strip_tags(htmlspecialchars($_POST['message']));
 
 // Create the email and send the message
-$to = "yourname@yourdomain.com"; // Add your email address in between the "" replacing yourname@yourdomain.com - This is where the form will send a message to.
-$subject = "Website Contact Form:  $name";
-$body = "Has recibido un nuevo correo de tu sitio web.\n\n"."Detalles:\n\nName: $name\n\nEmail: $email\n\nPhone: $phone\n\nMensaje:\n$message";
-$header = "From: noreply@tcu-residencia.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
+$to = "ariielpb@gmail.com"; // Add your email address in between the "" replacing yourname@yourdomain.com - This is where the form will send a message to.
+$subject = "Correo de:  $name";
+$body = "Has recibido un nuevo correo de tu sitio web.\n\n"."Detalles:\n\Nombres: $name\n\nEmail: $email\n\nMensaje:\n$message";
+$header = "De residenciadevida-tcu\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
 $header .= "Reply-To: $email";
 
 if(!mail($to, $subject, $body, $header))
   http_response_code(500);
-?>
